93a0ed8391e5f90ca731956f365b76e81c2ead1c
tahasama <taha.maatof@gmail.com>
2024-07-04 12:43:48 +0100

    starting with assessment page

--- app/(tabs)/Assessments.tsx
+++ app/(tabs)/Assessments.tsx
@@ -1,22 +1,81 @@
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import React from "react";
-import { StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { Image } from "expo-image";
 
-const Assessments = () => {
+import { useNavigation } from "@react-navigation/native";
+import assessment from "../../assets/images/assessment/assessment.jpg";
+import { LinearGradient } from "expo-linear-gradient";
+
+const AssessmentScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText>Assessments</ThemedText>
-    </ThemedView>
+    <ScrollView>
+      <Image
+        style={styles.image}
+        source={assessment}
+        // placeholder={{ blurhash }}
+        contentFit="contain"
+        transition={1000}
+      />
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["#D5FFFD", "#f5f5f5"]}
+        style={{ height: 120 }}
+      />
+      <View style={styles.container}>
+        <View style={styles.explanationContainer}>
+          <Text style={styles.explanationText}>
+            Embrace a consistent approach to unlock your full potential and
+            enhance productivity.
+          </Text>
+          <Text style={styles.explanationText}>
+            Discover diverse categories of activities designed for daily and
+            weekly assessments.
+          </Text>
+          <Text style={styles.highlightText}>
+            Aim to complete activities across all categories daily to maintain
+            progress and achieve your goals.
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    // backgroundColor: "white",
+    paddingHorizontal: 16,
+    paddingVertical: 36,
+    backgroundColor: "#f5f5f5",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    // marginBottom: 12,
+    // borderRadius: 8,
+  },
+  explanationContainer: {
+    marginBottom: 20,
+    marginTop: -160,
+    paddingHorizontal: 15,
+  },
+  explanationText: {
+    fontSize: 16,
+    color: "#555",
+    marginBottom: 10,
+  },
+  highlightText: {
+    color: "red",
+    fontWeight: "bold",
   },
 });
 
-export default Assessments;
+export default AssessmentScreen;
